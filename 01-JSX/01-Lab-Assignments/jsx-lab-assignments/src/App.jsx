2c85328f2be665007a4f45ecdbb5480e41497dc7
Pradeep Khatri <pradeepkhatri44@live.com>
2025-08-15 15:00:34 +0530

    commit JSX-Lab-Assignments
    commit JSX-Lab-Assignments

---
+++ 01-JSX/01-Lab-Assignments/jsx-lab-assignments/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  const topic = "JSX";
+
+  return (
+    <>
+      <h2>Welcome to JSX</h2>
+      <p>
+        With {topic}, you can use curly braces {"{}"} to insert dynamic data
+        including functions, expressions or variables inside the Javascript code
+        in the HTML syntaxes.
+      </p>
+    </>
+  );
+}
+
+export default App;
